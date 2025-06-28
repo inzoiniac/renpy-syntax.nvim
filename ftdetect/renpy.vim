7137a583b8672b24c7d59fc2a08a758b9f35c6ad
netom <neto.matsuoka@gmail.com>
2025-06-27 21:36:25 -0300

    Refatora plugin para usar apenas Lua e remove ftdetect

--- ftdetect/renpy.vim
+++
@@ -1,4 +0,0 @@
-augroup filetypedetect
-  autocmd!
-  autocmd BufRead,BufNewFile *.rpy setfiletype renpy
-augroup END
--- lua/renpy-syntax/init.lua
+++ lua/renpy-syntax/init.lua
@@ -1,15 +1,24 @@
--- Define o filetype quando abrir .rpy
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = "*.rpy",
-  callback = function()
-    vim.bo.filetype = "renpy"
-  end,
-})
+local M = {}
 
--- Define commentstring para o filetype renpy
-vim.api.nvim_create_autocmd("filetype", {
-  pattern = "renpy",
-  callback = function()
-    vim.bo.commentstring = "# %s"
-  end,
-})
+function M.setup()
+  -- Define o filetype renpy para arquivos .rpy
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*.rpy",
+    callback = function()
+      vim.bo.filetype = "renpy"
+    end,
+  })
+
+  -- Define o commentstring
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "renpy",
+    callback = function()
+      vim.bo.commentstring = "# %s"
+    end,
+  })
+
+  -- Carrega o syntax manualmente
+  vim.cmd("runtime! syntax/renpy.vim")
+end
+
+return M
